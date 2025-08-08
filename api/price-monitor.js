361c6e6c4ba5eedbee0d619aa62d0f1dac86631a
Anisul Islam <anis.inbox10@gmail.com>
2025-08-08 15:31:58 +0500

    new

--- api/price-monitor.js
+++ api/price-monitor.js
@@ -1,4 +1,4 @@
-import { checkPrices } from '../../services/PriceMonitor';
+import { checkPrices } from '../services/priceMonitor.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
